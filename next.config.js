16756ca93813bc221c86339c4205f6eba069a83c
Trịnh Hữu Tâm <trinhhuutam@gmail.com>
2024-04-14 16:46:31 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,5 @@ module.exports = {
   output: "export",
   images: {
     unoptimized: true,
-    formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-      },
-    ],
   },
 };
